b949085d8367fdb174ea1a4277c746cbc6092854
HaoHui Oh <ouhaohui@outlook.com>
2015-12-01 16:36:42 +0800

    update loadModule and view details in touch.md

--- static/src/loadModule/config.js
+++ static/src/loadModule/config.js
@@ -1,7 +1,7 @@
 (function(win){
-var flieMap = function(){
+var fileMap = function(){
     this.config = {
-        "basePath":"static/",
+        "basePath":"/static/",
         global:{
             "jquery":{
                 "version":"v1.11.2",
@@ -20,7 +20,7 @@ var flieMap = function(){
                 "css":[]
             },
             "test":{
-                version:"v0.1",
+                version:"v0.20",
                 "js":["test","maintest"],
                 "css":["boostrap-css","test-css"]
             }
@@ -40,18 +40,18 @@ var flieMap = function(){
     
     return this.initData();
 }
-    flieMap.prototype={
+    fileMap.prototype={
         initData:function(){//初始化改页面需要的 模块配置
             this.htmlName = win.htmlName || "";
             this.moduleFileSet={};
             var config = this.config;
             for(var key in config.global){
                // console.log(key)
-                var jsonNode = this.JsonNode(config.global[key],["js","css"]);
+                var jsonNode = this.JsonNode(config.global[key],key,["js","css"]);
                 this.moduleFileSet[key] = jsonNode;
                 //console.log(this.JsonNode(config.global[key],["js","css"]))
             }
-            this.moduleFileSet[this.htmlName] =this.JsonNode(config.modules[this.htmlName],["js","css"]);
+            this.moduleFileSet[this.htmlName] =this.JsonNode(config.modules[this.htmlName],this.htmlName,["js","css"]);
             /* for(var key in this.moduleFileSet)
              {
                 console.log(key);
@@ -61,47 +61,58 @@ var flieMap = function(){
         getBasePath:function(){//返回路径
             return this.config.basePath;
         },
-        getUpdateFileSet:function(needupdate){//根据检测版本返回的数组 返回需要更新的模块 url的json
-            var cssFlieSet = [],
-                jsFlieSet = [],
-                resultSet = {"js":[],"css":[]};
+        getNeedLoadFileSet:function(){//根据检测版本返回的数组 返回需要更新的模块 url的json
+            var needupdate = this.getNeedLoadModuleName();//[模块名]
+                resultSet = {};
             
             var moduleFileSet = this.moduleFileSet;
             for(var index in needupdate){
-                var js = moduleFileSet[needupdate[index]]["js"],
-                    css = moduleFileSet[needupdate[index]]["css"];
-                if(js){
-                    jsFlieSet = jsFlieSet.concat(js);
+                var fileSet = [],
+                    moduleName = needupdate[index],
+                    modulefile = moduleFileSet[moduleName];
+                if(modulefile){
+                    fileSet = fileSet.concat(modulefile);
                 }
-                if(css){
-                    cssFlieSet = cssFlieSet.concat(css);
+                
+                for(var index2 in fileSet){
+                var url =this.getBasePath() + this.config.modulesMap[fileSet[index2]];
+                if(!(resultSet[moduleName] instanceof Array)){
+                    resultSet[moduleName] = [];
                 }
+                resultSet[moduleName] = resultSet[moduleName].concat(url);
             }
-            for(var index in jsFlieSet){
-                var url = this.config.modulesMap[jsFlieSet[index]];
-                resultSet.js = resultSet.js.concat(url);
-            }
-            for(var index in cssFlieSet){
-                var url = this.config.modulesMap[cssFlieSet[index]];
-                resultSet.css = resultSet.css.concat(url);
             }
             return resultSet;
         },
         getMapValue:function(key){//根据模块名值返回 改模块的url
             return this.config.modulesMap[key];
         },
-        JsonNode:function(jsonObj,node){//重新组合 一个新的json ，node为 要从jsonObj中提取的节点
+            /**  
+            * 客户端向服务端发送请求  
+            * @param jsonObj:目标json对象  
+            * @param newObjName:新的json名字  
+            * @param node:从jsonObj哪些节点提取组合
+            */
+        JsonNode:function(jsonObj,newObjName,node){
             var innerJson = jsonObj,
                 node = node || [],
-                resultJson = {};
+                resultJson = [];
             for(var index in node){
-              resultJson[node[index]] = jsonObj[node[index]];
+                if(jsonObj[node[index]]){
+                   // console.log(jsonObj[node[index]]);
+                   resultJson = resultJson.concat(jsonObj[node[index]]);
+                   // console.log(resultJson)
+                };
+              
             }
-            //console.log(resultJson)
+            
             return resultJson;
             
         },
-        getVersion:function(){//获取模块版本json
+            /**  
+            * 获取模块版本json 
+            */
+        getVersion:function(){
             var resultSet = {};
             
             var config = this.config;
@@ -112,11 +123,19 @@ var flieMap = function(){
             resultSet[this.htmlName]=config.modules[this.htmlName].version
             
             return resultSet;
+        },
+        getNeedLoadModuleName:function(){
+            var resultSet = [];
+            var config = this.config;
+             for(var key in config.global){
+                    resultSet.push(key)
+            }
+            resultSet.push(this.htmlName);
+            return resultSet;
         }
-    
         
     }
-    win.flieMap = new flieMap();
+    win.fileMap = new fileMap();
    
 })(window)
 
